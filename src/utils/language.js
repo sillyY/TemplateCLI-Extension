6829bbbc9c8cbcdc6565b6952ea1cd604d52453b
sillyy <965768042@qq.com>
2019-10-18 11:01:35 +0800

    feat add language

---
+++ src/utils/language.js
@@ -0,0 +1,22 @@
+const CHINESE = {
+    ADD_INPUT: '输入框架、文件路径和文件名. 格式: <框架> <文件路径> [文件名(不带格式)...]',
+    ADD_SUCCESS: '文件创建成功',
+    INSERT_INPUT: '输入指定模板',
+    INSERT_SUCCESS: '插入文件成功',
+    REMOVE_INPUT: '输入文件路径和文件名. 格式: <文件路径> [文件名...]',
+    REMOVE_SUCCESS: '删除成功'
+}
+
+const ENGLISH = {
+    ADD_INPUT: 'Enter the frame, file path, and file name. Format: <framework> <file path> [file name (without format)...]',
+    ADD_SUCCESS: 'Create file successfully',
+    INSERT_INPUT: 'Enter the specified template',
+    INSERT_SUCCESS: 'Insert file successfully',
+    REMOVE_INPUT: 'Enter the file path and file name. Format: <file path> [file name...]',
+    REMOVE_SUCCESS: 'Deleted file successfully'
+}
+
+module.exports = {
+    CHINESE,
+    ENGLISH
+}
