8a9cc87ebb3fae645b008b838a7e9ab4b74e497e
sillyy <965768042@qq.com>
2019-10-18 13:12:01 +0800

    refactor remove useless code

--- src/modules/language.js
+++ src/modules/language.js
@@ -1,5 +1,5 @@
 const vscode = require('vscode')
-const { ALL_LANGUAGE, setLanguage, getLanguageConfig, LANGUAGE_CURRENT } = require('../config/language')
+const { ALL_LANGUAGE, setLanguage, getLanguageConfig } = require('../config/language')
 
 const lan = vscode.commands.registerCommand('extension.lan', async () => {
     const lans = Object.values(ALL_LANGUAGE),
